10d3140d481f6a9c04fcca0dd760f2de83a23ece
Ahmed Galal <117813417+Gallillio@users.noreply.github.com>
2023-03-29 18:54:50 +0200

    forgot to add the new index.php for Day-4

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<a href="add_employee/add-employee-form.php"> Head to adding Employee Record</a>
+<a href="admin/add_employee/add-employee-form.php"> Head to adding Employee Record</a>
 <br><br>
-<a href="add_menu_item/add-menu-item-form.php"> Head to adding new Food Item</a>
+<a href="admin/add_menu_item/add-menu-item-form.php"> Head to adding new Food Item</a>
